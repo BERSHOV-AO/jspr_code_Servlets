744d66b033e5bf65f015dd89e728d2b4b6c76209
Aleksey Bershov <bershov.aleksey@bk.ru>
2023-10-16 13:58:17 +0300

    Intermediate commit + code comments + test

--- src/main/java/ru/netology/controller/PostController.java
+++ src/main/java/ru/netology/controller/PostController.java
@@ -7,10 +7,17 @@ import ru.netology.service.PostService;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.io.Reader;
+import java.util.ArrayList;
+import java.util.List;
 
 public class PostController {
   public static final String APPLICATION_JSON = "application/json";
   private final PostService service;
+
+  //------------------------------------------
+  // Создаем лист для проверки
+   List<Post> postList = List.of(new Post(1,"11"),new Post(2,"12"));
+  //------------------------------------------
 
   public PostController(PostService service) {
     this.service = service;
@@ -18,23 +25,41 @@ public class PostController {
 
   public void all(HttpServletResponse response) throws IOException {
     response.setContentType(APPLICATION_JSON);
-    final var data = service.all();
+    //final var data = service.all();
+    //-------------------------
+    // проверка
+    final var data = postList;
+    //-------------------------
     final var gson = new Gson();
     response.getWriter().print(gson.toJson(data));
   }
 
-  public void getById(long id, HttpServletResponse response) {
+  public void getById(long id, HttpServletResponse response) throws IOException {
+    //---------------------------------------------------
+    // проверка
+    int tempId = (int)id;
+    final var dataId = postList.get(tempId);
+    final var gson = new Gson();
+    response.getWriter().print(gson.toJson(dataId));
+    //---------------------------------------------------
+
     // TODO: deserialize request & serialize response
   }
 
+  // тело запроса и ответ
   public void save(Reader body, HttpServletResponse response) throws IOException {
     response.setContentType(APPLICATION_JSON);
     final var gson = new Gson();
     final var post = gson.fromJson(body, Post.class);
+    // Метод save() возвращает тот post который мы получили
+    // Условно сохраняем, на самом деле возвращаем пользователю
+    // В домашнем задании надо именно сохранять.
     final var data = service.save(post);
+    // Будем JSON преобразовывать методом toJson()
     response.getWriter().print(gson.toJson(data));
   }
 
+  // Удаление по ID
   public void removeById(long id, HttpServletResponse response) {
     // TODO: deserialize request & serialize response
   }
--- src/main/java/ru/netology/servlet/MainServlet.java
+++ src/main/java/ru/netology/servlet/MainServlet.java
@@ -1,14 +1,17 @@
 package ru.netology.servlet;
 
 import ru.netology.controller.PostController;
+import ru.netology.model.Post;
 import ru.netology.repository.PostRepository;
 import ru.netology.service.PostService;
 
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
 
 public class MainServlet extends HttpServlet {
+
 //  private PostController controller;
 //
 //  @Override
@@ -18,6 +21,85 @@ public class MainServlet extends HttpServlet {
 //    controller = new PostController(service);
 //  }
 
+    private PostController controller;
+
+    @Override
+    public void init() {
+        // Создаем репозиторий
+        final var repository = new PostRepository();
+        // Создаем сервис
+        final var service = new PostService(repository);
+        // Создаем контроллер
+        controller = new PostController(service);
+    }
+//--------------------------------------------------------------------------------
+//    @Override
+//    protected void service(HttpServletRequest req, HttpServletResponse resp) {
+//        // Выставим статус для нашего запроса по методу GET
+//        // Какой статус
+//        //resp.setStatus(HttpServletResponse.SC_CONFLICT);
+//        resp.setStatus(HttpServletResponse.SC_ACCEPTED);
+//        // какой контент
+//        resp.setContentType("text/plain");
+//        try {
+//            // Попробовать вывести на экран браузера
+//            resp.getWriter().println("Hello from MainServlet!");
+//        } catch (IOException e) {
+//            throw new RuntimeException(e);
+//        }
+//    }
+//--------------------------------------------------------------------------------
+
+    @Override
+    protected void service(HttpServletRequest req, HttpServletResponse resp) {
+        try {
+            // Получаем путь
+            var path = req.getRequestURI();
+            // Получаем метод
+            var method = req.getMethod();
+            // Примитивная маршрутизация
+            if (method.equals("GET") && path.equals("/api/posts")) {
+                // Если пришел метод GET И путь "/api/posts", то мы хотим получить все посты которые есть в системе
+                // Вызываем метод controller.all(resp);
+                controller.all(resp);
+                return;
+            }
+            // Если пришел метод GET и /api/posts/\\d+" и любое число \\d+"
+            // То мы хотим получить информацию о каком то конкретном посте
+            // И именно этот id, мы и буде перредовать в метод controller.getById(id, resp);
+            if (method.equals("GET") && path.matches("/api/posts/\\d+")) {
+                // Простой способ
+                var id = Long.parseLong(path.substring(path.lastIndexOf("/")));
+                // И именно этот id, мы и буде перредовать в метод controller.getById(id, resp);
+                controller.getById(id, resp);
+                return;
+            }
+            // Так же метод POST, Если пришел метод POST И путь "/api/posts",
+            if (method.equals("POST") && path.equals("/api/posts")) {
+                // Вызываем метод save(), с помощью getReader() - получаем тело, которое нам пришло
+                controller.save(req.getReader(), resp);
+            }
+
+            // Метод DELETED, удаляем по конкретному (id - \\d+) "/api/posts/\\d+"
+            if (method.equals("DELETED") && path.matches("/api/posts/\\d+")) {
+                // Простой способ
+                var id = Long.parseLong(path.substring(path.lastIndexOf("/")));
+                controller.removeById(id, resp);
+                return;
+            }
+            // Ели пришло что то другое, выбрасываем 404 ошибку
+            resp.setStatus(HttpServletResponse.SC_NOT_FOUND);
+
+        } catch (Exception e) {
+            e.printStackTrace();
+            // Если исключение то 500 ошибка
+            resp.setStatus(HttpServletResponse.SC_INTERNAL_SERVER_ERROR);
+        }
+
+    }
+
+
+//--------------------------------------------------------------------------------
 //  @Override
 //  protected void service(HttpServletRequest req, HttpServletResponse resp) {
 //    // если деплоились в root context, то достаточно этого
